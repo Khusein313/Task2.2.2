8f43ebe8e62bffa22a45754a6835c612a6e67c56
Khusein Navruzov <khusein.academy@gmail.com>
2025-11-01 16:34:19 +0300

    first commit

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,10 +1,11 @@
 package web.service;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import web.model.Car;
 import java.util.Arrays;
 import java.util.List;
 
-@Service
+@Component
 public class CarServiceImpl {
     List<Car> cars = Arrays.asList(new Car(1,"AUDI","New"),
             new Car(2,"BMW","Broken"),
